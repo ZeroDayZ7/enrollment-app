4ead55ff4458a6f90c8f9d49ad74d9dd7880cc19
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-14 18:17:19 +0200

    Refactor official login and dashboard components; remove citizen components and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,14 @@
 import { Routes } from '@angular/router';
-import { CitizenDashboardComponent } from '../features/citizen/citizen-dashboard/citizen-dashboard.component';
-import { CitizenLoginComponent } from '../features/citizen/citizen-login/citizen-login.component';
 import { OfficialDashboardComponent } from '../features/official/official-dashboard/official-dashboard.component';
 import { OfficialLoginComponent } from '../features/official/official-login/official-login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'citizen/login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-  // Strefa Obywatela
-  { path: 'citizen/login', component: CitizenLoginComponent },
-  { path: 'citizen/dashboard', component: CitizenDashboardComponent },
-
-  // Strefa Urzędnika
-  { path: 'official/login', component: OfficialLoginComponent },
-  { path: 'official/dashboard', component: OfficialDashboardComponent },
+  // Panel Urzędnika
+  { path: 'login', component: OfficialLoginComponent },
+  { path: 'dashboard', component: OfficialDashboardComponent },
 
   // Fallback
-  { path: '**', redirectTo: 'citizen/login' }
+  { path: '**', redirectTo: 'login' }
 ];
