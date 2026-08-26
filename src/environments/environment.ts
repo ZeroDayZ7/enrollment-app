a2f0c6d3606be9e60d97d4fd214b10c392342090
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-26 16:47:55 +0200

    feat(crypto-service): add Base64 decoding utility and update signChallenge method to include domain binding feat(environment): add domain configuration to development environment feat(environment.prod): create production environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
+// cmdr: src\environments\environment.ts
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8090/api/v1'
+  apiUrl: 'http://localhost:8090/api/v1',
+  domain: 'obywatel.gov.pl'
 };
